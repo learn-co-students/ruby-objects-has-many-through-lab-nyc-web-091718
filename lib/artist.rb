a65123789733963510b7719bc9a7f5c3b3790903
kristopherbullinger <github email address>
2018-09-20 11:57:47 -0400

    Lab Complete

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Artist
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def genres
+    Song.all.map {|song| song.genre}
+  end
+
+end
